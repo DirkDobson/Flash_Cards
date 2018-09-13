4b99fdd80c742ab9f341bd8faddd4d7c91090da6
Dirk Dobson <dirkvancouver@live.com>
2018-09-12 19:15:21 -0600

    inital commit

---
+++ app/index.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    modules: ['app', 'node_modules'],
+    extensions: [".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        include: [ path.join(__dirname, "app")],
+        options: { cacheDirectory: true }
+      }
+     ]
+   },
+   devtool: 'source-map'
+};
+
+
+const button = document.getElementsByClassName("example");
+
